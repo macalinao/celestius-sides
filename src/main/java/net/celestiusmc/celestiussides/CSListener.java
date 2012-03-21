4d586e51edc84ab46a5a9b87175e2011a0d46f10
Ian Macali <ianmacalinao@gmail.com>
2012-03-21 00:40:00 -0500

    Set race when clicking on sign.

--- src/main/java/net/celestiusmc/celestiussides/CSListener.java
+++ src/main/java/net/celestiusmc/celestiussides/CSListener.java
@@ -4,9 +4,12 @@
  */
 package net.celestiusmc.celestiussides;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -16,31 +19,50 @@ import org.bukkit.event.player.PlayerInteractEvent;
  * Celestius Sides plugin
  */
 public class CSListener implements Listener {
+
+    public CelestiusSides plugin;
+
+    public CSListener(CelestiusSides plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler(ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event) {
         Block block = event.getClickedBlock();
         if (block == null) {
             return;
         }
-        
+
         Action action = event.getAction();
         if (!action.equals(Action.RIGHT_CLICK_BLOCK)) {
             return;
         }
-        
+
         BlockState state = block.getState();
         if (!(state instanceof Sign)) {
             return;
         }
-        
+
         Sign sign = (Sign) state;
         String raceLine = sign.getLine(2);
         Race race = Race.fromString(raceLine);
         if (race == null) {
             return;
         }
+
+        Player player = event.getPlayer();
+        RaceManager rm = plugin.getRaceManager();
+        Race theirRace = rm.getRace(player.getName());
+        if (!theirRace.equals(Race.NONE)) {
+            player.sendMessage(ChatColor.RED + "You are already of the race '"
+                    + theirRace.getNiceName() + "'.");
+            return;
+        }
         
-        //TODO set race
+        rm.setRace(player.getName(), race);
+        player.sendMessage(ChatColor.YELLOW + "You are now of the race '" + race.getNiceName() + "'.");
+        Location raceSpawn = rm.getSpawnLocation(race);
+        player.teleport(raceSpawn);
     }
 
 }
--- src/main/java/net/celestiusmc/celestiussides/CelestiusSides.java
+++ src/main/java/net/celestiusmc/celestiussides/CelestiusSides.java
@@ -11,10 +11,13 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Celestius sides main plugin class.
  */
 public class CelestiusSides extends JavaPlugin {
+
     private static CelestiusSides _instance;
-    
+
+    private CSListener listener;
+
     private RaceManager raceManager;
-    
+
     @Override
     public void onDisable() {
         _instance = null;
@@ -24,12 +27,22 @@ public class CelestiusSides extends JavaPlugin {
     @Override
     public void onEnable() {
         _instance = this;
+        listener = new CSListener(this);
         raceManager = new RaceManager(this);
-        getServer().getPluginManager().registerEvents(new CSListener(), this);
+        getServer().getPluginManager().registerEvents(listener, this);
         getLogger().log(Level.INFO, "Plugin enabled.");
+    }
+
+    public CSListener getListener() {
+        return listener;
+    }
+
+    public RaceManager getRaceManager() {
+        return raceManager;
     }
 
     public static CelestiusSides getInstance() {
         return _instance;
     }
+
 }
